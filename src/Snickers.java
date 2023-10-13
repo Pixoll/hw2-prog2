f8cd9394aa5602a928c8a772ff81281ab806cb92
Pixoll <pixoll.gamer@gmail.com>
2023-10-13 00:19:25 -0300

    here have a snickers
    you're not your when you're hungry

---
+++ src/Snickers.java
@@ -0,0 +1,9 @@
+public class Snickers extends Dulce {
+    public Snickers(int serie) {
+        super(serie);
+    }
+
+    public String comer() {
+        return "here have a snickers, you're not you when you're hungry";
+    }
+}
